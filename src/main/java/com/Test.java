29d16275bc28625f90035a09b553545fa598da47
Jeckwu <2517708125@qq.com>
2020-10-29 10:54:41 +0800

    jeckwu的提交

---
+++ src/main/java/com/Test.java
@@ -0,0 +1,23 @@
+package com;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+    public static void main(String[] args) {
+
+        System.out.println("1");
+        List<Object> arrayList = new ArrayList<>();
+        arrayList.add(1);
+        boolean add = arrayList.add(new Object());
+        System.out.println("1");
+        for (Object o : arrayList) {
+            System.out.println(o);
+        }
+
+
+    }
+    private static final int SUM = 1;
+
+
+}
